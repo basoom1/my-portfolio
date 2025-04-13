c8c983c5de105764ae03e2a33742623f5881a42c
Basoom1 <basoomoubari@gmail.com>
2025-04-13 06:24:08 +0300

    Final build for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // Ensures assets are served correctly
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
+
 });
